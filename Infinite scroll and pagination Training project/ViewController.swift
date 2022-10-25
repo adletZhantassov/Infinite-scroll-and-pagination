1790e2a77b899ea527c6cf8a14a672a120ae030b
Adlet <adlet_1996@mail.ru>
2022-10-25 23:03:27 +0600

    Initial Commit

---
+++ Infinite scroll and pagination Training project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Infinite scroll and pagination Training project
+//
+//  Created by Adlet Zhantassov on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
